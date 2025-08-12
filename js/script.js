8431a419f1b2c25f3a37587e5bbbead42502b15b
Sitara Shahzad <sitara.shahzad@devsspace.com>
2025-08-12 15:46:37 +0500

    second commit

---
+++ js/script.js
@@ -0,0 +1,199 @@
+const popularCars = [
+   {
+      name: "Koenigsegg",
+      type: "Sport",
+      likedImg: "assets/images/popular-car/Like.png",
+      carImg: "assets/images/popular-car/car1.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "90L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "2 People" }
+      ],
+      price: "$99.00/"
+    },
+    {
+      name: "Nissan GT - R",
+      type: "Sport",
+      likedImg: "assets/images/popular-car/heart.png",
+      carImg: "assets/images/popular-car/car-2.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "2 People" }
+      ],
+      price: "$80.00/"
+    },
+    {
+      name: "Rolls - Royce",
+      type: "Sedan",
+      likedImg: "assets/images/popular-car/Like.png",
+      carImg: "assets/images/popular-car/car3.png",
+      carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "70L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "4 People" }
+      ],
+      price: "$96.00/"
+    },
+     {
+      name: "Nissan GT - R",
+      type: "Sport",
+      likedImg: "assets/images/popular-car/heart.png",
+      carImg: "assets/images/popular-car/car-2.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "2 People" }
+      ],
+      price: "$80.00/"
+    }
+  ];
+
+  const recommendationCars = [
+    {
+      name: "All New Rush",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/heart.png",
+      carImg: "assets/images/Recomended-cars/RecCar1.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "70L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "6 People" }
+      ],
+      price: "$72.00/"
+    },
+    {
+      name: "CR  - V",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/like.png",
+      carImg: "assets/images/Recomended-cars/RecCar2.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "6 People" }
+      ],
+      price: "$80.00/"
+    },
+    {
+      name: "All New Terios",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/heart.png",
+      carImg:"assets/images/Recomended-cars/Car1.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "90L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "6 People" }
+      ],
+      price: "$80.00/"
+    },
+     {
+      name: "CR  - V",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/like.png",
+      carImg: "assets/images/Recomended-cars/Car3.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "6 People" }
+      ],
+      price: "$80.00/"
+    },
+     {
+      name: "MG ZX Exclusice",
+      type: "Hatchback",
+      likedImg: "assets/images/popular-car/like.png",
+      carImg: "assets/images/Recomended-cars/Car4.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "70L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "4 People" }
+      ],
+      price: "$76.00/"
+    },
+     {
+      name: "New MG ZS",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/like.png",
+      carImg:"assets/images/Recomended-cars/Car5.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "4 People" }
+      ],
+      price: "$80.00/"
+    },
+     {
+      name: "MG ZX Excite",
+      type: "Hatchback",
+      likedImg: "assets/images/popular-car/like.png",
+      carImg: "assets/images/Recomended-cars/Car6.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "4 People" }
+      ],
+      price: "$74.00/"
+    },
+     {
+      name: "New MG ZS",
+      type: "SUV",
+      likedImg: "assets/images/popular-car/heart.png",
+      carImg: "assets/images/popular-car/car3.png",
+       carshaddow:"assets/images/popular-car/shadow.png",
+      specs: [
+        { img: "assets/images/popular-car/gas-station.png", text: "80L" },
+        { img: "assets/images/popular-car/Car-wheel.png", text: "Manual" },
+        { img: "assets/images/popular-car/profile-2user.png", text: "6 People" }
+      ],
+      price: "$80.00/"
+    },
+
+  ];
+
+  function renderCards(data, containerId) {
+    const container = document.getElementById(containerId);
+    data.forEach(car => {
+      const cardHTML = `
+        <div class="card box-shaddow column gap-1">
+          <div class="card-head row space-between">
+            <div class="card-heading Plus-Jakarta-Sans column gap-1">
+              <h2>${car.name}</h2>
+              <p>${car.type}</p>
+            </div>
+            <img src="${car.likedImg}" alt="like">
+          </div>
+          <div class="card-content column gap-3">
+            <div class="car-image"><img src="${car.carImg}" alt="${car.name}"></div>
+            <div class="carshaddow"><img src="${car.carshaddow}" alt="${car.name}"></div>
+            <div class="car-details row space-between">
+              ${car.specs.map(spec => `
+                <div class="car-prop row align-center gap-1">
+                  <img class="car-prop-image" src="${spec.img}" alt="">
+                  <h4 class="car-prop-text">${spec.text}</h4>
+                </div>
+              `).join('')}
+            </div>
+            <div class="car-price-details row space-between">
+              <div class="car-price">${car.price}</div>
+              <div class="btn card-btn"><button>Rent Now</button></div>
+            </div>
+          </div>
+        </div>
+      `;
+      container.innerHTML += cardHTML;
+    });
+  }
+
+  renderCards(popularCars, "popularCarsContainer");
+  renderCards(recommendationCars, "recommendationCarsContainer");
